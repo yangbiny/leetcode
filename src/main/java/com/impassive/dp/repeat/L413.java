0b52b664f7d2e52791fe796b23f59a203e77216d
impassive <tingfa.yang@duitang.com>
2021-01-11 21:15:45 +0800

    add leetcode repeat 413

---
+++ src/main/java/com/impassive/dp/repeat/L413.java
@@ -0,0 +1,29 @@
+package com.impassive.dp.repeat;
+
+/** @author impassivey */
+public class L413 {
+
+  public static void main(String[] args) {
+    int[] nums = {1, 2, 3, 4};
+    System.out.println(new Solution().numberOfArithmeticSlices(nums));
+  }
+
+  static class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+      if (A.length < 3) {
+        return 0;
+      }
+      int[] dp = new int[A.length + 1];
+      dp[0] = 0;
+      dp[1] = 0;
+      int res = 0;
+      for (int i = 2; i < A.length; i++) {
+        if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+          dp[i] = dp[i - 1] + 1;
+          res += dp[i];
+        }
+      }
+      return res;
+    }
+  }
+}
