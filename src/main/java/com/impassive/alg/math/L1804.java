bf0be16970137359ab9b3837ffdb183afdd57ea7
impassive <tingfa.yang@duitang.com>
2021-03-01 21:30:14 +0800

    add leetcode 1804

--- src/main/java/com/impassive/alg/math/L1804.java
+++ src/main/java/com/impassive/alg/math/L1804.java
@@ -5,7 +5,7 @@ public class L1804 {
 
   public static void main(String[] args) {
     int[] check = {1, 2, 4, 2, 3, 1, 2, 4, 5, 1};
-    System.out.println(new Solution().findRepeatNumber(check));
+    System.out.println(new Solution().findRepeatNumberV2(check));
   }
 
   static class Solution {
@@ -23,6 +23,21 @@ public class L1804 {
           return num;
         }
         sb.replace(num, num + 1, "1");
+      }
+      return 0;
+    }
+
+    public int findRepeatNumberV2(int[] nums) {
+      int n = nums.length;
+      for (int i = 0 ; i < n;i++){
+        int k = nums[i];
+        if (k < 0){
+          k += n;
+        }
+        if (nums[k] < 0){
+          return k;
+        }
+        nums[k] -= n;
       }
       return 0;
     }
