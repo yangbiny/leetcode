27aafb0a485aea40a93651b82c14003cfc22a454
impassive <tingfa.yang@duitang.com>
2021-03-03 21:30:19 +0800

    add leetcode 1143

--- src/main/java/com/impassive/alg/dp/repeat/L1143.java
+++ src/main/java/com/impassive/alg/dp/repeat/L1143.java
@@ -4,28 +4,29 @@ package com.impassive.alg.dp.repeat;
 public class L1143 {
 
   public static void main(String[] args) {
-    System.out.println(new Solution().longestCommonSubsequence("abcd", "scde"));
+    System.out.println(new Solution().longestCommonSubsequence("abcd", "abcd"));
   }
 
   static class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-      int m = text1.length();
-      int n = text2.length();
-      if (m == 0 || n == 0) {
+      if (text1.length() == 0 || text2.length() == 0) {
         return 0;
       }
-
-      int[][] dp = new int[m + 1][n + 1];
-      for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= i && j <= n; j++) {
+      int[][] dp = new int[text1.length() + 1][text1.length() + 1];
+      int max = 0;
+      for (int i = 1; i <= text1.length(); i++) {
+        for (int j = 1; j <= text2.length(); j++) {
           if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
             dp[i][j] = dp[i - 1][j - 1] + 1;
           } else {
             dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
           }
+          if (dp[i][j] > max) {
+            max = dp[i][j];
+          }
         }
       }
-      return dp[m][n];
+      return max;
     }
   }
 }
