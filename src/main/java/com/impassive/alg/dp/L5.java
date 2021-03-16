df2afced4b3e3df66000b2582608775fbdc80845
impassive <tingfa.yang@duitang.com>
2021-03-16 13:58:54 +0800

    add leetcode 5

--- src/main/java/com/impassive/alg/dp/L5.java
+++ src/main/java/com/impassive/alg/dp/L5.java
@@ -4,7 +4,7 @@ package com.impassive.alg.dp;
 public class L5 {
 
   public static void main(String[] args) {
-    System.out.println(new Solution().longestPalindrome("ba"));
+    System.out.println(new Solution().longestPalindrome("baab"));
   }
 
   static class Solution {
@@ -12,30 +12,26 @@ public class L5 {
       if (s.length() == 0 || s.length() == 1) {
         return s;
       }
-      String result = s.charAt(0)+"";
+      String result = s.charAt(0) + "";
       int n = s.length();
-      for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-          String substring = s.substring(i, j + 1);
-          if (check(substring)) {
-            result = result.length() > substring.length() ? result : substring;
-          }
-        }
+      int start = 0;
+      while (start < n) {
+        String temp = check(s, start, start);
+        String temp1 = check(s, start, start + 1);
+        result = result.length() > temp.length() ? result : temp;
+        result = result.length() > temp1.length() ? result : temp1;
+        start++;
       }
       return result;
     }
 
-    private boolean check(String s) {
-      int n = s.length() - 1;
-      int i = 0;
-      while (i < n) {
-        if (s.charAt(i) != s.charAt(n)) {
-          return false;
-        }
-        i++;
-        n--;
+    private String check(String s, int start, int end) {
+      int n = s.length();
+      while (start >= 0 && end < n && s.charAt(start) == s.charAt(end)) {
+        start--;
+        end++;
       }
-      return true;
+      return s.substring(start + 1, end);
     }
   }
 }
