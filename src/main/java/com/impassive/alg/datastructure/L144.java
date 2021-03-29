9a2d1746cf591da33477acb3c92d3c070f12aa8f
impassive <tingfa.yang@duitang.com>
2021-03-29 21:24:32 +0800

    add leetcode 144

--- src/main/java/com/impassive/alg/datastructure/L144.java
+++ src/main/java/com/impassive/alg/datastructure/L144.java
@@ -3,6 +3,7 @@ package com.impassive.alg.datastructure;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 /** @author impassivey */
 public class L144 {
@@ -15,12 +16,28 @@ public class L144 {
         return Collections.emptyList();
       }
       List<Integer> result = new ArrayList<>();
-      process(root, result);
+      Stack<TreeNode> treeNodes = new Stack<>();
+      treeNodes.add(root);
+      while (!treeNodes.empty()) {
+        final TreeNode pop = treeNodes.pop();
+        if (pop == null){
+          continue;
+        }
+        treeNodes.push(pop.right);
+        treeNodes.push(pop.left);
+        result.add(pop.val);
+      }
       return result;
     }
 
+    /**
+     * 递归遍历
+     *
+     * @param root 根节点
+     * @param result 结果集
+     */
     private void process(TreeNode root, List<Integer> result) {
-      if (root == null){
+      if (root == null) {
         return;
       }
       result.add(root.val);
