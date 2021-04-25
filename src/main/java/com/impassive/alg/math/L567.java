0bc50afe4f02c41118323ad9fb55d7de6547a3de
impassive <tingfa.yang@duitang.com>
2021-04-25 22:13:59 +0800

    add leetcode 567

--- src/main/java/com/impassive/alg/math/L567.java
+++ src/main/java/com/impassive/alg/math/L567.java
@@ -1,6 +1,7 @@
 package com.impassive.alg.math;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 /** @author impassivey */
 public class L567 {
@@ -53,3 +54,40 @@ public class L567 {
     }
   }
 }
+
+/*class Solution {
+  public boolean checkInclusion(String s1, String s2) {
+    if (s1.length() == 0) {
+      return true;
+    }
+    boolean result = false;
+    int s1Len = s1.length();
+    final int length = s2.toCharArray().length;
+    for (int i = 0; i < length; i++) {
+      if (s1.contains(s2.charAt(i) + "")) {
+        String temp;
+        if (i + s1Len <= length) {
+          temp = s2.substring(i, i + s1Len);
+          result = check(s1, temp);
+        }
+        if (!result && i - s1Len >= 0) {
+          temp = s2.substring(i - s1Len, i);
+          result = check(s1, temp);
+        }
+      }
+      if (result){
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean check(String s1, String temp) {
+    final char[] chars = s1.toCharArray();
+    final char[] chars1 = temp.toCharArray();
+    Arrays.sort(chars);
+    Arrays.sort(chars1);
+    return Objects.equals(new String(chars), new String(chars1));
+  }
+}*/
