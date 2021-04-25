cfd83ddd7eb1c35c56d1d86dc9dac64a35212a7d
impassive <tingfa.yang@duitang.com>
2021-04-25 22:20:11 +0800

    add leetcode 567

--- src/main/java/com/impassive/alg/math/L567.java
+++ src/main/java/com/impassive/alg/math/L567.java
@@ -7,53 +7,79 @@ import java.util.Objects;
 public class L567 {
 
   public static void main(String[] args) {
-    System.out.println(new Solution().checkInclusion("abc", "bac"));
+    System.out.println(new Solution().checkInclusion("abc", "dddbac"));
   }
 
   static class Solution {
     public boolean checkInclusion(String s1, String s2) {
-      if (s1.length() == 0) {
+      int n = s1.length(), m = s2.length();
+      if (n > m) {
+        return false;
+      }
+      int[] cnt1 = new int[26];
+      int[] cnt2 = new int[26];
+      for (int i = 0; i < n; ++i) {
+        ++cnt1[s1.charAt(i) - 'a'];
+        ++cnt2[s2.charAt(i) - 'a'];
+      }
+      if (Arrays.equals(cnt1, cnt2)) {
         return true;
       }
-      boolean result = false;
-      int s1Len = s1.length();
-
-      int[] chars = new int[26];
-      for (char c : s1.toCharArray()) {
-        ++chars[c - 'a'];
-      }
-
-      final int length = s2.toCharArray().length;
-      for (int i = 0; i < length; i++) {
-        if (s1.contains(s2.charAt(i) + "")) {
-          String temp;
-          if (i + s1Len <= length) {
-            temp = s2.substring(i, i + s1Len);
-            result = check(chars, temp);
-          }
-          if (!result && i - s1Len >= 0) {
-            temp = s2.substring(i - s1Len, i);
-            result = check(chars, temp);
-          }
-        }
-        if (result) {
+      for (int i = n; i < m; ++i) {
+        ++cnt2[s2.charAt(i) - 'a'];
+        --cnt2[s2.charAt(i - n) - 'a'];
+        if (Arrays.equals(cnt1, cnt2)) {
           return true;
         }
       }
-
       return false;
-    }
-
-    private boolean check(int[] chars, String temp) {
-      int[] tempChar = new int[26];
-      final char[] chars1 = temp.toCharArray();
-      for (char c : chars1) {
-        ++tempChar[c - 'a'];
-      }
-      return Arrays.equals(tempChar, chars);
     }
   }
 }
+
+/*class Solution {
+  public boolean checkInclusion(String s1, String s2) {
+    if (s1.length() == 0) {
+      return true;
+    }
+    boolean result = false;
+    int s1Len = s1.length();
+
+    int[] chars = new int[26];
+    for (char c : s1.toCharArray()) {
+      ++chars[c - 'a'];
+    }
+
+    final int length = s2.toCharArray().length;
+    for (int i = 0; i < length; i++) {
+      if (s1.contains(s2.charAt(i) + "")) {
+        String temp;
+        if (i + s1Len <= length) {
+          temp = s2.substring(i, i + s1Len);
+          result = check(chars, temp);
+        }
+        if (!result && i - s1Len >= 0) {
+          temp = s2.substring(i - s1Len, i);
+          result = check(chars, temp);
+        }
+      }
+      if (result) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean check(int[] chars, String temp) {
+    int[] tempChar = new int[26];
+    final char[] chars1 = temp.toCharArray();
+    for (char c : chars1) {
+      ++tempChar[c - 'a'];
+    }
+    return Arrays.equals(tempChar, chars);
+  }
+}*/
 
 /*class Solution {
   public boolean checkInclusion(String s1, String s2) {
