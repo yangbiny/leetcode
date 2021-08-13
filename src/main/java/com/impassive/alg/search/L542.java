9b34b3d4b4f108a40764a68adbb9e9c93464a346
impassive <tingfa.yang@duitang.com>
2021-08-13 18:41:22 +0800

    add leetcode L542

--- src/main/java/com/impassive/alg/search/L542.java
+++ src/main/java/com/impassive/alg/search/L542.java
@@ -33,70 +33,31 @@ public class L542 {
       for (int[] ints : dp) {
         Arrays.fill(ints, -1);
       }
-      LinkedList<Pair> queue = new LinkedList<>();
-      boolean first = false;
+      Queue<int[]> queue = new LinkedList<>();
       for (int i = 0; i < length; i++) {
         for (int j = 0; j < yLength; j++) {
           if (mat[i][j] == 0) {
             dp[i][j] = 0;
-          } else {
-            Pair e = new Pair(i, j);
-            if (!first) {
-              if (i - 1 > 0 && mat[i - 1][j] == 0) {
-                first = addFirst(queue,e);
-              } else if (j - 1 > 0 && mat[i][j - 1] == 0) {
-                first = addFirst(queue,e);
-              } else if (i + 1 < length && mat[i + 1][j] == 0) {
-                first = addFirst(queue, e);
-              } else if (j + 1 < yLength && mat[i][j + 1] == 0) {
-                first = addFirst(queue,e);
-              } else {
-                queue.add(e);
-              }
-            } else {
-              queue.add(e);
-            }
+            queue.add(new int[] {i, j});
           }
         }
       }
+
       while (!queue.isEmpty()) {
-        Pair poll = queue.poll();
-        int min = Integer.MAX_VALUE;
+        int[] poll = queue.poll();
+        int x = poll[0], y = poll[1];
+
         for (int[] ints : direction) {
-          int x = ints[0] + poll.x;
-          int y = ints[1] + poll.y;
-          if (x < 0 || y < 0 || x >= length || y >= yLength) {
+          int i = ints[0] + x;
+          int j = ints[1] + y;
+          if ((i < 0 || j < 0 || i >= length || j >= yLength) || dp[i][j] != -1) {
             continue;
           }
-          if (dp[x][y] == -1) {
-            continue;
-          }
-          min = Math.min(min, dp[x][y]);
+          dp[i][j] = dp[x][y] + 1;
+          queue.add(new int[] {i, j});
         }
-        dp[poll.x][poll.y] = min + 1;
       }
       return dp;
-    }
-
-    private boolean addFirst(LinkedList<Pair> queue, Pair e) {
-      if (queue.isEmpty()){
-        queue.add(e);
-        return true;
-      }
-      Pair pair = queue.get(0);
-      queue.set(0, e);
-      queue.add(pair);
-      return true;
-    }
-  }
-
-  static class Pair {
-    int x;
-    int y;
-
-    public Pair(int x, int y) {
-      this.x = x;
-      this.y = y;
     }
   }
 }
